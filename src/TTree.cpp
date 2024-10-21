399bebd5049b44000ea3fccc1dccbb00025454f1
Marco Visani <92217074+mvisani@users.noreply.github.com>
2024-10-21 17:08:44 +0200

    updated tree loading. Still need to implement unit tests

--- src/TTree.cpp
+++ src/TTree.cpp
@@ -4,9 +4,7 @@
 
 #include "TTree.h"
 #include "coretools/Files/TInputFile.h"
-#include <iostream>
-#include <sstream>
-#include <utility>
+#include <string>
 
 // Tree destructor implementation
 TTree::~TTree() = default;
@@ -19,30 +17,56 @@ void TTree::load_from_file(const std::string &filename) {
 		UERROR("File '", filename, "' is expected to have 3 columns, but has ", file.numCols(), " !");
 	}
 
-	//
+	// read each line of the file
 	for (; !file.empty(); file.popFront()) {
-		bool is_root        = true;
-		size_t parent_index = 0;
-		if (file.get(1) != "NA") {
-			// we read line by line the edge list from the file
-			// if the child node (column 0) is not in the tree, we add the node,
-			// if the parent is not in the tree, we add the node and set the parent as root
-			// as long as it has no parents.
-			//
-			//
-			// auto parent = std::find(_nodes.begin(), _nodes.end(), file.get(1));
-			// if (parent == _nodes.end()) {
-			// 	UERROR("Parent '", file.get(1), "' of node '", file.get(0), "' does not exist !");
-			// }
-			// TODO implement our way
-			parent_index = parent - _nodes.begin();
+		// we read line by line the edge list from the file
+		// if the child node (column 0) is not in the tree, we add the node,
+		// if the parent is not in the tree, we add the node and set the parent as root
+		// as long as it has no parents.
+		std::string child    = std::string(file.get(0));
+		std::string parent   = std::string(file.get(1));
+		double branch_length = file.get<double>(2);
+
+		if (!in_tree(child) && !in_tree(parent)) {
+			_nodes.emplace_back(parent, -1, -1, true);
+			TNode parent_node  = get_node(parent);
+			auto node_iterator = std::find(_nodes.begin(), _nodes.end(), parent_node.id());
+			int parent_index   = node_iterator - _nodes.begin();
+			_nodes.emplace_back(child, branch_length, parent_index, false);
+			_nodes[parent_index].addChild(_nodes.size() - 1);
+		} else if (!in_tree(child) && in_tree(parent)) {
+			TNode parent_node  = get_node(parent);
+			auto node_iterator = std::find(_nodes.begin(), _nodes.end(), parent_node.id());
+			int parent_index   = node_iterator - _nodes.begin();
+			// we add the child node to the tree
+			_nodes.emplace_back(child, branch_length, parent_index, false);
+			_nodes[parent_index].addChild(_nodes.size() - 1);
+		} else if (in_tree(child) && !in_tree(parent)) {
+			// if the child node was in the tree but not the parent
+			// that means that the child was a root and is now becoming
+			// a child of the new parent which will be added to the tree
+			TNode child_node   = get_node(child);
+			auto node_iterator = std::find(_nodes.begin(), _nodes.end(), child_node.id());
+			int child_index    = node_iterator - _nodes.begin();
+			_nodes[child_index].set_is_root(false);
+			_nodes[child_index].set_branch_length_to_parent(branch_length);
+			_nodes[child_index].set_parent_index(_nodes.size());
+			_nodes.emplace_back(parent, -1, -1, true);
+			_nodes[_nodes.size() - 1].addChild(child_index);
+		} else {
+			// if both nodes are in the tree we just need to add the child to the parent relationship
+			// and set the different requirements
+			TNode child_node     = get_node(child);
+			TNode parent_node    = get_node(parent);
+			auto child_iterator  = std::find(_nodes.begin(), _nodes.end(), child_node.id());
+			auto parent_iterator = std::find(_nodes.begin(), _nodes.end(), parent_node.id());
+			int child_index      = child_iterator - _nodes.begin();
+			int parent_index     = parent_iterator - _nodes.begin();
+			_nodes[child_index].set_is_root(false);
+			_nodes[child_index].set_branch_length_to_parent(branch_length);
+			_nodes[child_index].set_parent_index(parent_index);
+			_nodes[parent_index].addChild(child_index);
 		}
-
-		auto branch_length = file.get<double>(2);
-		_nodes.emplace_back(file.get(0), branch_length, parent_index, is_root);
-
-		// add child to parent
-		if (!is_root) { _nodes[parent_index].addChild(_nodes.size() - 1); }
 	}
 
 	// identify roots and leaves
@@ -68,4 +92,10 @@ TNode TTree::get_node(std::string &Id) {
 	if (node_iterator == _nodes.end()) { UERROR("Node '", Id, "' does not exist !"); }
 	int node_index = node_iterator - _nodes.begin();
 	return _nodes[node_index];
+}
+
+bool TTree::in_tree(const std::string &node) {
+	auto node_iterator = std::find(_nodes.begin(), _nodes.end(), node);
+	if (node_iterator == _nodes.end()) { return false; }
+	return true;
 }
--- src/TTree.h
+++ src/TTree.h
@@ -14,11 +14,11 @@
 
 class TNode {
 private:
-	std::string _id;                   // unique identifier for the node
-	size_t _parentIndex;               // pointer to parent node
-	double _branchLengthToParent;      // branch length to parent node
-	size_t _discretised_branch_length; // discretised branch length
-	std::vector<size_t> _children;     // vector to child nodes indices
+	std::string _id;                     // unique identifier for the node
+	size_t _parentIndex;                 // pointer to parent node
+	double _branchLengthToParent;        // branch length to parent node
+	size_t _discretised_branch_length{}; // discretised branch length
+	std::vector<size_t> _children;       // vector to child nodes indices
 	bool _is_root;
 
 public:
@@ -38,6 +38,9 @@ public:
 	[[nodiscard]] bool isLeaf() const { return _children.empty(); };
 	[[nodiscard]] bool isRoot() const { return _is_root; };
 	[[nodiscard]] double branchLengthToParent() const { return _branchLengthToParent; };
+	void set_is_root(bool is_root) { _is_root = is_root; }
+	void set_branch_length_to_parent(double branch_length) { _branchLengthToParent = branch_length; }
+	void set_parent_index(size_t parent_index) { _parentIndex = parent_index; }
 
 	bool operator==(const std::string &Id) const { return _id == Id; };
 };
@@ -68,5 +71,7 @@ public:
 
 	// Method to get all the root nodes
 	[[nodiscard]] const std::vector<size_t> &get_root_nodes() const { return _roots; }
+
+	bool in_tree(const std::string &node);
 };
 #endif // METABOLITE_INFERENCE_TREE_H
